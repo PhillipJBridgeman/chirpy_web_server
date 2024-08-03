c5b1012e62df5362b4ba556a0f95afabd724222c
Phillip Bridgeman <phillipbridgeman@gmail.com>
2024-08-03 16:22:51 -0500

    Add readiness endpoint and update fileserver path

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir(".")) // Serve files from the current directory
 
-	mux.Handle("/", fileServer)
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	mux.Handle("/healthz", http.HandlerFunc(healthzHandler))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 
 	server := &http.Server{
 		Addr:    "localhost:8080",
